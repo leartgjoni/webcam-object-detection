da8b286c7437b983e96c37cb7cc52bb3c7074c65
Leart <lgjoni95@gmail.com>
2019-02-19 14:52:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "@tensorflow/tfjs";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    model: null,
+    stream: null,
+    videoElement: null,
+    canvasElement: null
+  };
+  async componentDidMount() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          facingMode: "user"
+        },
+        audio: false
+      });
+
+      const model = await cocoSsd.load();
+
+      await this.setState({
+        videoElement: this.refs.video,
+        canvasElement: this.refs.canvas,
+        stream,
+        model
+      });
+
+      this.state.videoElement.srcObject = this.state.stream;
+      this.predictFrame();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  predictFrame = async () => {
+    const predictions = await this.state.model.detect(this.refs.video);
+    this.drawPredictions(predictions);
+    //recursive call
+    this.predictFrame();
+  };
+
+  drawPredictions = predictions => {
+    const ctx = this.state.canvasElement.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw prediction box.
+      ctx.strokeStyle = "#fa00ff";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+      // Draw text box.
+      ctx.fillStyle = "#fa00ff";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+      // Draw text.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <video
+          className="position"
+          autoPlay
+          playsInline
+          muted
+          ref="video"
+          width="600"
+          height="500"
+        />
+        <canvas className="position" ref="canvas" width="600" height="500" />
       </div>
     );
   }
